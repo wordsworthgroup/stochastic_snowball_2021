f0eeb9b31e6db7969d29e471679aa011efa3ac69
Wordsworth Group <10161055+wordsworthgroup@users.noreply.github.com>
2021-03-18 21:50:59 -0400

    Add files via upload

--- make_Fig_1.m
+++ make_Fig_1.m
@@ -41,11 +41,23 @@ if fresh_start
     dS   = climate.S0*(alpha-1);
     y    = @(x) exp((climate.a*x-dS)/climate.b);
     dydx = @(x) climate.a*y(x)/climate.b;
-    q    = @(y) solver.q(y,chi,sig_y);
-    p    = @(x) q(y(x)).*dydx(x);
+    %q    = @(y) solver.q(y,chi,sig_y);
+    %p    = @(x) q(y(x)).*dydx(x);
+    q_ana = solver.q(y_h,chi,sig_y);
+    p_ana = solver.q(y(x_h),chi,sig_y).*dydx(x_h);
 
     % find a case where snowball transition occurred
     [it,ir_snow] = find(x_a<climate.xs & solver.y_a>2e-2,1);
+    
+    % save arrays for plotting
+    t_a = solver.t_a;
+    y_i = solver.y_a(:,ir_snow);
+    x_i = x_a(:,ir_snow);
+    ym_a = zeros(nt,1) + mean(solver.y_a(:,ir_snow))*climate.fCO20;
+    xm_a = zeros(nt,1) + mean(x_a(:,ir_snow)) + climate.T0;
+    
+    % save some space
+    clear solver x_a
 
 else
     
@@ -53,21 +65,17 @@ else
 
 end
 
-% calculate mean quantities
-ym_a = zeros(nt,1) + mean(solver.y_a(:,ir_snow))*climate.fCO20;
-xm_a = zeros(nt,1) + mean(x_a(:,ir_snow)) + climate.T0;
-
 % display fCO2 time series
 subplot(2,2,1)
-plot(solver.t_a,solver.y_a(:,ir_snow)*climate.fCO20,'k'); hold on
-plot(solver.t_a,ym_a,'Color',[0.5 1 0.5]*0.75,'LineWidth',1.5)
+plot(t_a,y_i*climate.fCO20,'k'); hold on
+plot(t_a,ym_a,'Color',[0.5 1 0.5]*0.75,'LineWidth',1.5)
 xlabel('t [My]'); ylabel('f_{CO2} [ppmv]')
 axis([0 500 0 2*climate.chi*climate.fCO20])
 
 % display T time series
 subplot(2,2,3)
-plot(solver.t_a,x_a(:,ir_snow)+climate.T0,'k'); hold on
-plot(solver.t_a,xm_a,'Color',[0.5 1 0.5]*0.85,'LineWidth',1.5)
+plot(t_a,x_i+climate.T0,'k'); hold on
+plot(t_a,xm_a,'Color',[0.5 1 0.5]*0.85,'LineWidth',1.5)
 xlabel('t [My]'); ylabel('T [K]')
 h = patch([0 500 500 0], [278 278 280 280],'c','LineStyle','none');
 set(h,'facealpha',.2)
@@ -76,13 +84,13 @@ axis([0 500 278 290])
 
 % display fCO2 pdf
 subplot(2,2,2)
-plot(q_num,fCO2_h,'k',q(y_h)/climate.fCO20,fCO2_h,'r--')
+plot(q_num,fCO2_h,'k',q_ana/climate.fCO20,fCO2_h,'r--')
 xlabel('q(f_{CO2}) [1/ppmv]'); ylabel('f_{CO2} [ppmv]')
 axis([0 3e-3 0 2*climate.chi*climate.fCO20])
 
 % display T pdf
 subplot(2,2,4)
-semilogx(p_num,T_h,'k',p(x_h),T_h,'r--')
+semilogx(p_num,T_h,'k',p_ana,T_h,'r--')
 xlabel('p(T) [1/K]'); ylabel('T [K]')
-axis([1e-6 1 278 292])
-xticks([1e-6 1e-5 1e-4 1e-3 1e-2 1e-1 1])
+axis([1e-5 1 278 292])
+xticks([1e-5 1e-4 1e-3 1e-2 1e-1 1])
